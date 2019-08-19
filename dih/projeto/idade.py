8f59c0fbbc2b69df33fdd461ab683c448fd0836b
ADS <ADS@DESKTOP-LL7I4ML>
2019-08-19 19:48:45 -0300

    primeira versao

---
+++ dih/projeto/idade.py
@@ -0,0 +1,16 @@
+'''
+Created on 19 de ago de 2019
+
+@author: ADS
+'''
+
+if __name__ == '__main__':
+
+    idade= 14
+    if idade >=18:
+        print("adulto")
+    else:
+        if idade >= 13:
+            print("adolecente")
+        else:
+            print("crianca")
